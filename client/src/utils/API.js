961870a302a1bbfbe4f8b657e6c6c094cca9f718
Niki Catapano <nsc9605@gmail.com>
2021-03-25 14:12:30 -0400

    protect api key .env file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,16 +4,16 @@ const options = {
   method: 'GET',
   url: 'https://the-cocktail-db.p.rapidapi.com/filter.php',
   headers: {
-    'x-rapidapi-key': '76154b20a6msh1c46e6b8ab8b5b5p1b1615jsn5419bb2d731f',
-    'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com'
+    'x-rapidapi-key': process.env.API_KEY,
+    'x-rapidapi-host': process.env.API_HOST
   }
 };
 
 // fetch("https://the-cocktail-db.p.rapidapi.com/filter.php", {
 // 	"method": "GET",
 // 	"headers": {
-// 		"x-rapidapi-key": "76154b20a6msh1c46e6b8ab8b5b5p1b1615jsn5419bb2d731f",
-// 		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
+// 		"x-rapidapi-key": process.env.API_KEY,
+// 		"x-rapidapi-host": process.env.API_HOST
 // 	}
 // })
 // .then(response => {
