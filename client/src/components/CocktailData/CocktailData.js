517cc23930f72bf65dc8dcc4f0f128d72ab2820d
Niki Catapano <nsc9605@gmail.com>
2021-03-25 21:53:16 -0400

    start API call and create a page for data

---
+++ client/src/components/CocktailData/CocktailData.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import options from '../../utils/API';
+
+
+
+function CocktailData() {
+
+    const cocktailData = []
+
+    const []
+
+    useEffect(() => {
+        getCocktails();
+      });
+    
+      function getCocktails() {
+        options.search()
+        .then(results => {
+
+            const data = {
+
+            }
+            // set(results)
+            cocktailData.push(data);
+        })
+        return(cocktailData);
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+    }
+export default CocktailData;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,31 +1,22 @@
-// import axios from "axios";
+import axios from "axios";
+require("dotenv").config();
 
-// const options = {
-//   method: 'GET',
-//   url: 'https://the-cocktail-db.p.rapidapi.com/filter.php',
-//   headers: {
-//     'x-rapidapi-key': process.env.API_KEY,
-//     'x-rapidapi-host': process.env.API_HOST
-//   }
-// };
+const options = {
+  method: "GET",
+  url: "https://the-cocktail-db.p.rapidapi.com/filter.php",
+  headers: {
+    "x-rapidapi-key": process.env.API_KEY,
+    "x-rapidapi-host": process.env.API_HOST,
+  },
+};
 
-fetch("https://the-cocktail-db.p.rapidapi.com/filter.php", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": process.env.API_KEY,
-		"x-rapidapi-host": process.env.API_HOST
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
-
+export default options;
