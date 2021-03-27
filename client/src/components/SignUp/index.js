84df41fba759e27b3b6e16e3c2b89952dd076ceb
Russell Jewett <rfjewett3@gmail.com>
2021-03-27 12:03:58 -0400

    updates login credentials to work correctly.

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import auth from "../../Providers/UserProvider";
+import { auth } from "../../firebase";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -35,45 +35,42 @@ const SignUp = () => {
   };
 
   return (
-    <div className="mt-8">
-      <h1 className="text-3xl mb-2 text-center font-bold">Sign Up</h1>
-      <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
+    <div>
+      <h1>Sign Up</h1>
+      <div>
         {error !== null && (
-          <div className="py-4 bg-red-600 w-full text-white text-center mb-3">
+          <div>
             {error}
           </div>
         )}
-        <form className="">
-          <label htmlFor="displayName" className="block">
+        <form>
+          <label htmlFor="displayName">
             Display Name:
           </label>
           <input
             type="text"
-            className="my-1 p-1 w-full "
             name="displayName"
             value={displayName}
             placeholder="Your Name"
             id="displayName"
             onChange={event => onChangeHandler(event)}
           />
-          <label htmlFor="userEmail" className="block">
+          <label htmlFor="userEmail">
             Email:
           </label>
           <input
             type="email"
-            className="my-1 p-1 w-full"
             name="userEmail"
             value={email}
             placeholder="example@gmail.com"
             id="userEmail"
             onChange={event => onChangeHandler(event)}
           />
-          <label htmlFor="userPassword" className="block">
+          <label htmlFor="userPassword">
             Password:
           </label>
           <input
             type="password"
-            className="mt-1 mb-3 p-1 w-full"
             name="userPassword"
             value={password}
             placeholder="Your Password"
@@ -81,23 +78,20 @@ const SignUp = () => {
             onChange={event => onChangeHandler(event)}
           />
           <button
-            className="bg-green-400 hover:bg-green-500 w-full py-2 text-white"
             onClick={event => {
               createUserWithEmailAndPasswordHandler(event, email, password);
             }}
           >
-            <Link to="/">Sign up</Link>
+            Sign up
           </button>
         </form>
-        <p className="text-center my-3">or</p>
-        <button
-          className="bg-red-500 hover:bg-red-600 w-full py-2 text-white"
-        >
+        <p>or</p>
+        <button>
           Sign In with Google
         </button>
-        <p className="text-center my-3">
+        <p>
           Already have an account?{" "}
-          <Link to="/" className="text-blue-500 hover:text-blue-600">
+          <Link to="/">
             Sign in here
           </Link>
         </p>
