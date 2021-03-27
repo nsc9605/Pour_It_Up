49762fbd3fd99ab5156afb8dc1776a7e50937521
Niki Catapano <nsc9605@gmail.com>
2021-03-27 13:25:44 -0400

    comment out favorite page

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import CocktailData from '../components/CocktailData/CocktailData';
+// import React from 'react';
+// import CocktailData from '../components/CocktailData/CocktailData';
 
-function Favorites() {
+// function Favorites() {
 
-    return(
+//     return(
 
-        <CocktailData />
+//         <CocktailData />
          
 
-    return (
-        <div>
-            { /*show favorites saved on this page*/}
-        </div>
+//     return (
+//         <div>
+//             { /*show favorites saved on this page*/}
+//         </div>
 
-    )
-}
+//     )
+// }
 
-export default Favorites;
+// export default Favorites;
