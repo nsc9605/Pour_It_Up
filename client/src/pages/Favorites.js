1ccc550ed211f5a7856524888732b45d50a67b7d
Russell Jewett <rfjewett3@gmail.com>
2021-03-28 00:35:30 -0400

    SignIn with google now works. Re-direct still needs work

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -2,18 +2,14 @@ import React from 'react';
 import CocktailData from '../components/CocktailData/CocktailData';
 
 function Favorites() {
-
     return(
-
-        <CocktailData />
-         
-
-    return (
-        <div>
-            { /*show favorites saved on this page*/}
-        </div>
-
-    )
+        <>
+            <CocktailData />
+            <div>
+                { /*show favorites saved on this page*/}
+            </div>
+        </>
+    );
 }
 
 export default Favorites;
