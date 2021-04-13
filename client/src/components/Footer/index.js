50b66c7c2fe4ff7a1bfa0d0c9cfbadb781b924c9
Russell Jewett <rfjewett3@gmail.com>
2021-04-12 21:39:18 -0400

    replaces love with a heart in footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Footer() {
   return (
     <div className="holder">
-      <h3 id="footTitle">Made With Love By:</h3>
+      <h3 id="footTitle">Made With &#10084; By:</h3>
       <h4 id="contributers">
         <a href="https://github.com/nsc9605">Niki</a>,
         <a href="https://github.com/deck-jessica"> Jessica</a>, 
